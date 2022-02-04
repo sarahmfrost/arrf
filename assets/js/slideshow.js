b06105b17dd7784edaff69f7b74400e896982154
Sarah <smfrost@ucsc.edu>
2022-02-03 20:34:05 -0800

    Update slideshow.js

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -35,10 +35,13 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
+    slides[i].style.text-align = "center";
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.text-align = "center";
+
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
