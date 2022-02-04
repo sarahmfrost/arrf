4581596acb0f6f591900da891ab565e208a6e77f
Sarah <smfrost@ucsc.edu>
2022-02-03 21:00:08 -0800

    updating donation info

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -35,13 +35,11 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.textAlign = "center";
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  slides[slideIndex-1].style.textAlign = "center";
 
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
